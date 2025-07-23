237eb4c86ed7fb11fe7792ec73af48b2aa1bfc36
dhaval-narale <dhavalnarale45@gmail.com>
2025-07-23 22:28:02 +0530

    Add Ansible deployment playbook and update EC2 provisioning in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -29,6 +29,20 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_vpc.id
 }
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow SSH and HTTP"
