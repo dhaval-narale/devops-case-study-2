5f5d347ab0a2e6594ff2676c16ee293d95beca85
dhaval-narale <145041906+dhaval-narale@users.noreply.github.com>
2025-09-03 09:02:14 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,11 @@
-output "instance_id" {
-  value = aws_instance.web.id
+output "ec2_pub_ip"{
+    value = aws_eip.my_eip.public_ip
 }
 
-output "public_ip" {
-  value = aws_eip.web_ip.public_ip
+output "ec2_pub_dns"{
+    value = aws_instance.my_instance.public_dns
+}
+
+output "ec2_private_ip" {
+    value = aws_instance.my_instance.private_ip
 }
