69cb369d33909e6a98c0109ea720ab3474632937
dhaval-narale <145041906+dhaval-narale@users.noreply.github.com>
2025-09-17 19:20:34 +0530

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 GIT_COMMIT=${GIT_COMMIT:-latest}
 IMAGE="dhavalnarale/devops-nodejs-app:$GIT_COMMIT"
+LATEST_IMAGE="dhavalnarale/devops-nodejs-app:latest"
 
 echo "Building Docker Image: $IMAGE"
 docker build -t "$IMAGE" .
+docker tag "$IMAGE" "$LATEST_IMAGE"
 
 echo "Pushing Docker Image to DockerHub: $IMAGE"
-docker push "$IMAGE"
+docker push "$LATEST_IMAGE"
