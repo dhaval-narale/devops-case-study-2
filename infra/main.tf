92da0fac0d5470951bb6b5070d8e68da344968db
dhaval-narale <145041906+dhaval-narale@users.noreply.github.com>
2025-11-04 16:37:38 +0530

    Change availability zone for default subnet

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,7 @@ resource "aws_default_vpc" "default"{
 # Default Subnet
 
 resource "aws_default_subnet" "default_az1" {
-    availability_zone = "us-east-1"
+    availability_zone = "us-east-1a"
 }
 
 # Security Group
